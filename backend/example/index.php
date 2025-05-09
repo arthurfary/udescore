0527c1e734957310654c35568bf9bcfbf66e1cbd
Fooyer <xneerd157@gmail.com>
2025-05-08 22:02:39 -0300

    melhoria no example para incluir como utilizar o DB

--- backend/example/index.php
+++ backend/example/index.php
@@ -1,9 +1,11 @@
 <?php
 
-require_once "../api.php";
+require_once "../api.php"; // Importa classe para API
+require_once "../db.php"; // Importa classe para DataBase
 
 try {
     $api = new Api(); // Inicializa a api
+    $db = new DB(); // Instancia o banco de dados
 
     $api->check_method("POST"); // Valida se o método chamado é o correto
 
@@ -13,19 +15,34 @@ try {
     $token = $api->obterHeader("Authorization"); // Obtém o valor de um header específico, nesse caso irá trazer o valor do Authorization
 
     $body = $api->obterBody(); // Obtém todas as informações do body da requisição em forma de array
-    // Exemplo: {nome:"example",id:"1"} será transformado em:
+    // Exemplo: {nome:"example"} será transformado em:
 
-    $id = $body["id"]; // Exemplo para obter o id no body da requisição
     $nome = $body["nome"]; // Exemplo para obter o nome no body da requisição
+
+    $db->TStart(); // Inicia uma transação no DB (Usar sempre para inputs ou update de informação no banco)
+
+    $query1 = "INSERT INTO tabela (nome) VALUES (:nome)"; // Define uma query para ser executada no banco de dados
+    $parameters = [":nome" => $nome]; // Em um array, define os parameters, ou seja, os :variavel que serão substituidos no sql.
+
+    $resposta1 = $db->query($query1, $parameters); // Chama a query criada com os parâmetros e armazena a resposta na variável $resposta1
+
+    $db->TCommit(); // Efetiva a transação no DB
+
+    // Query 2
+
+    $query2 = "SELECT * FROM tabela"; // Define uma query para ser executada no banco de dados
+
+    $resposta2 = $db->query($query2); // Chama a query criada e armazena a resposta na variável $resposta2
 
     $api->sendResponse(
         200, // Código http de retorno
         // Array de retorno
         [
             "success" => true, // booleano falando se a api executou com sucesso
-            "data" => "Exemplo", // Mensagem/dados de retorno
+            "data" => $resposta2, // Mensagem/dados de retorno (Nesse caso retorna o resultado da query)
         ]
     );
 } catch (Exception $e) {
+    $db->TRollback(); // Sempre quando tiver Tstart e TCommit precisa ter o TRollback no catch
     $api->tratarException($e); // Trata os erros dos métodos chamados durante a execução da api
 }
