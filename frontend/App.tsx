038a8ab098d6610adc6b8132da3916b3235fa0e7
striixx <92183292+striixx@users.noreply.github.com>
2025-06-01 21:03:18 -0300

    Pagina Login/cadastro/logo (#20)
    Closes #4

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -4,39 +4,13 @@ import {
   Text,
   View,
 } from 'react-native';
-
+import LoginScreen from './Pages/LoginScreen';
 
 function App(): React.JSX.Element {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Udescore :)</Text>
-    </View>
+    <LoginScreen />
   );
 }
 
-// exemplo css do default
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
