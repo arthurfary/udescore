f4cd7a50e9ebaa3f1b24c7d338403a16daff5a64
Fernando Nogueira <fernando.cossta@gmail.com>
2025-06-23 13:45:22 -0300

    Update the Welcome component with the new scaffolding

--- frontend/src/app/welcome/welcome.tsx
+++ frontend/src/app/welcome/welcome.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import useWelcome from './welcome.hook';
+import styles from './welcome.styles';
 
 export interface WelcomeProps {}
 
 const Welcome: React.FC<WelcomeProps> = () => {
   const {} = useWelcome();
   return (
-    <View>
-      <Text>Welcome Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome Screen</Text>
     </View>
   );
 };
